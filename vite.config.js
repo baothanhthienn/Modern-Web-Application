8addc3b41dd0dd0112871434c5b45cfe644f2921
Nguyen Gia Bao Pham <105292789@student.swin.edu.au>
2026-05-31 10:53:03 +1000

    kk
    kk

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/cos30043/s105292789/project/dist",
   plugins: [vue()],
 
   server: {
